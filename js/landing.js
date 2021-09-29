c7ee209bb88946a06fbc555477c98dd6103f285d
Sanparty <88841545+Sanparty@users.noreply.github.com>
2021-09-29 00:59:25 -0400

    Create landing.js

---
+++ js/landing.js
@@ -0,0 +1,26 @@
+const videotext = `
+VIDEO EDITING
+`;
+
+const webtext = `
+WEB DEVELOPMENT
+`;
+
+
+document.querySelector(".video-text").style.textAlign = "center"
+
+const videologo = document.querySelector(".video-text");
+
+videologo.innerHTML = videotext
+
+document.querySelector(".web-text").style.textAlign = "center"
+
+const weblogo = document.querySelector(".web-text");
+
+weblogo.innerHTML = webtext
+
+
+
+
+
+// <a href="#" class="video-link"><img src="img/video_editing_icon.svg" alt="Video Editing" class="field-logo"></a>
