7464a72aeeccbc0c92c899e4ce714ffbee9c31dc
Sanparty <88841545+Sanparty@users.noreply.github.com>
2021-10-27 17:01:16 -0400

    updated landing with js and styling

--- js/landing.js
+++ js/landing.js
@@ -1,16 +1,84 @@
 
-videologo.innerHTML = videotext
 
-document.querySelector(".web-text").style.textAlign = "center"
+let mainlogo = document.getElementById("logo_dsp");
 
-const logoText = document.querySelector(".video-text")
 
-logoText.innerHTML = 
-`<h1>WEB DEVELOPMENT`</h1>
-
-logoText.innerHTML = `
-<h1>VIDEO EDITING</h1>`
+window.onload = function(){
+    mainlogo.style.opacity = 1;
+    };
 
 
 
-// <a href="#" class="video-link"><img src="img/video_editing_icon.svg" alt="Video Editing" class="field-logo"></a>
+const logos  = () => {
+
+    
+
+    let webtext = document.querySelector(".web-text");
+
+    let videotext = document.querySelector(".video-text");
+
+    webtext.innerHTML = `<a href="index.html" class="web-link" target="_blank"><img src="img/web_icon_50.svg" id="web_image" alt="Web Development"></a>
+    `;
+
+    videotext.innerHTML = `<a href="http://dansanpedro.com/dspeditor/index.html" class="video-link" target="_blank"><img src="img/video_editing_icon.svg" id="video_image" alt="Video Editing"></a>
+    `;
+    let webimage = document.getElementById("web_image");
+    let videoimage = document.getElementById("video_image");
+    let mainlogo = document.getElementById("logo_dsp");
+    var socialmedia = document.querySelectorAll(".social-media_logo");
+    var i;
+
+
+window.onload = function(){
+    mainlogo.style.opacity = 1;
+   
+    setTimeout(function() {
+        webimage.style.opacity = 1;
+        videoimage.style.opacity = 1;
+      }, 1000);
+
+    setTimeout(function() {
+        for (i = 0; i < socialmedia.length; i++) {
+            socialmedia[i].style.opacity = 1;
+          }
+      }, 2000);
+ 
+    };
+    
+    let webmiddle = document.createElement("div");
+    webmiddle.classList.add(`middle`);
+    let webmiddletext = document.createElement("div");
+    webmiddletext.classList.add('text');
+    webmiddletext.innerHTML = `WEB DEVELOPMENT`;
+    webmiddle.append(webmiddletext);
+    webtext.append(webmiddle);
+
+    let videomiddle = document.createElement("div");
+    videomiddle.classList.add(`video-middle`);
+    let videomiddletext = document.createElement("div");
+    videomiddletext.classList.add('text');
+    videomiddletext.innerHTML = `VIDEO EDITING`;
+    videomiddle.append(videomiddletext);
+    videotext.append(videomiddle);
+
+    webtext.addEventListener("mouseover", function (event) {
+        webmiddletext.style.opacity = "1";
+        webmiddletext.style.textTransform = "uppercase";
+    })
+    webtext.addEventListener("mouseout", function (event) {
+        webmiddletext.style.opacity = "0";
+    })
+
+    videotext.addEventListener("mouseover", function (event) {
+        videomiddletext.style.opacity = "1";
+        videomiddletext.style.textTransform = "uppercase";
+    })
+    videotext.addEventListener("mouseout", function (event) {
+        videomiddletext.style.opacity = "0";
+    })
+
+
+
+};      
+
+logos();
