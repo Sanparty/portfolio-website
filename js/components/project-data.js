ee0f17faa2f3c3c99b846b6fb9d03d972f270afe
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-02-14 16:07:47 -0500

    added 2 new work projects and updated resume

--- js/components/project-data.js
+++ js/components/project-data.js
@@ -4,7 +4,7 @@ const webPortfolio = new Project(
   "DSP Portfolio",
   "Check out my portfolio, created during my Intro to Frontend Development course at Career Foundry. I learned about coding in HTML, applying styling with CSS, version control with Git, and much more.",
   "https://github.com/Sanparty/portfolio-website",
-  "http://dansanpedro.com/index.html",
+  "https://dansanpedro.com/index.html",
   "img/web_portfolio_square.png"
 );
 
@@ -12,7 +12,7 @@ const annWebsite = new Project(
     "Ann Nakajima, ND",
     "A professional website for my wife's Naturopathy business. The website is still in the early stages as we work together to create a website to promote her business to clients.",
     "none",
-    "http://dansanpedro.com/ann/ann_index.html",
+    "https://dansanpedro.com/ann/ann_index.html",
     "img/ann_website_square.png"
 );
 
@@ -28,22 +28,10 @@ const quizTimer = new Project(
     "Quiz Timer DSP",
     `I created this small web application after searching for a timer online to use during the test. I wanted to follow how much time I had remaining and also the pace I was keeping.`,
     "https://github.com/Sanparty/Quiz-Timer",
-    "http://dansanpedro.com/quiztimer",
+    "https://dansanpedro.com/quiztimer",
     "img/quiz_timer_square.png"
 );
 
-// OLD QUIZ TIMER DESCRIPTION
-// const quizTimer = new Project(
-//     "Quiz Timer DSP - JavaScript coding practice",
-//     `I created this small web application after taking an assessment test for a position in Web Development training at my current company.<br><br>
-
-//     I was searching for a timer online to use during the test, as it was a timed test with a set amount of questions. I wanted to follow how much time I had remaining but also at what pace I was finishing questions.<br><br>
-    
-//     This was very good practice in coding in JavaScript.`,
-//     "https://github.com/Sanparty/Quiz-Timer",
-//     "http://dansanpedro.com/quiztimer",
-//     "img/quiz_timer_square.png"
-// );
 
 const editwpPortfolio= new Project(
     "DSP Video Editing Portfolio - WordPress version",
@@ -53,9 +41,23 @@ const editwpPortfolio= new Project(
     "img/editing_wp_square.png"
 );
 
+const johnSite = new Project(
+    "John Manucdoc, Broker",
+    `Working on a refreshed website for a real estate broker. Early stages of design in 2023 using the Vue.js framework.`,
+    "none",
+    "https://dansanpedro.com/john",
+    "img/john_square.png"
+);
+const convertFilepath = new Project(
+    "Convert Your File Path",
+    `I created this single page application for work using the Vue.js framework, as some colleagues were having to manually convert filepaths from Mac to Windows and vice versa.`,
+    "https://github.com/Sanparty/Convert-Vue",
+    "https://dansanpedro.com/convert",
+    "img/convert_square.png"
+);
 
 
-const projectObjectArray = [webPortfolio, editPortfolio, annWebsite, quizTimer];
+const projectObjectArray = [webPortfolio, editPortfolio, johnSite, annWebsite, quizTimer, convertFilepath];
 
 // Export the array to be used in other files
 export default projectObjectArray;
